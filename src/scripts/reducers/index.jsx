0086615929680a13d2413f33f507b5c5432fe9cd
René Monroy <rene.monroy@gmail.com>
2016-07-21 15:30:03 -0500

    Combine reducers in an index file

---
+++ src/scripts/reducers/index.jsx
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+import { default as Progress } from './Progress';
+
+const CombinedReducers = combineReducers({ Progress });
+export default CombinedReducers;
