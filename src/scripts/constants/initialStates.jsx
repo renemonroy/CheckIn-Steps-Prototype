75d6f0c2d5543c5244ec4b429f68c1086cee9e53
René Monroy <rene.monroy@gmail.com>
2016-07-22 12:47:19 -0500

    Fix small issues in the initial state of snkrs

--- src/scripts/constants/initialStates.jsx
+++ src/scripts/constants/initialStates.jsx
@@ -1,6 +1,6 @@
 import { fromJS } from 'immutable';
 
-export const Snkrs = fromJS({
+export const snkrs = fromJS({
   1001: {
     title: 'ALL-STAR',
     subtitle: 'JORDAN HORIZON',
@@ -9,7 +9,7 @@ export const Snkrs = fromJS({
       default: 'https://cloud.githubusercontent.com/assets/106011/15694814/94bcd1bc-2766-11e6-9011-0f0edae7dce5.jpg',
       preload: 'https://cloud.githubusercontent.com/assets/106011/17034199/39d34f02-4f47-11e6-987b-99c0969a4f08.jpg',
     },
-    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
+    description: 'Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.',
   },
   1120: {
     title: 'OKC THUNDER AWAY GRADIENT',
@@ -19,7 +19,7 @@ export const Snkrs = fromJS({
       default: 'https://cloud.githubusercontent.com/assets/106011/15694823/9bdb1dfa-2766-11e6-8d02-80fb57912f46.jpg',
       preload: 'https://cloud.githubusercontent.com/assets/106011/17034209/417b9778-4f47-11e6-8ed0-79586edf415f.jpg',
     },
-    description: "This latest Nike KD 8 Elite player exclusive is the boldest take yet thanks to a fiery gradient midsole in direct contrast to the bright blue base.",
+    description: 'This latest Nike KD 8 Elite player exclusive is the boldest take yet thanks to a fiery gradient midsole in direct contrast to the bright blue base.',
   },
   1459: {
     title: 'TELEVISION',
@@ -29,6 +29,6 @@ export const Snkrs = fromJS({
       default: 'https://cloud.githubusercontent.com/assets/106011/15694826/a480f524-2766-11e6-95df-afbff18be17f.jpg',
       preload: 'https://cloud.githubusercontent.com/assets/106011/17034221/4b8cc002-4f47-11e6-94af-b29796f55dd2.jpg',
     },
-    description: "Nike Basketball is facing a post-playing career Kobe lineup from here on out. Next up is a grade school exclusive Kobe 11 featuring broadcast color bars in a nod to television.",
+    description: 'Nike Basketball is facing a post-playing career Kobe lineup from here on out. Next up is a grade school exclusive Kobe 11 featuring broadcast color bars in a nod to television.',
   },
 });
