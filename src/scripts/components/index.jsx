7a6f7f4172a418d1aa5b0b2c28067de97b53d3e2
René Monroy <rene.monroy@gmail.com>
2016-07-22 12:47:01 -0500

    Update App component to render scenes

--- src/scripts/components/index.jsx
+++ src/scripts/components/index.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
-const App = () =>
-  <div id="app">
-    <p>Just a simple text here...</p>
-  </div>;
+import React, { PropTypes, Component } from 'react';
+import { UIContent, UIScenesGroup } from './ui';
+
+class App extends Component {
+
+  static displayName = 'App';
+
+  static propTypes = {
+    children: PropTypes.any,
+    location: PropTypes.object,
+  };
+
+  render() {
+    const { children, location: loc } = this.props;
+    return (
+      <div id="app">
+        <UIContent>
+          <UIScenesGroup scenes={() => children} sceneRoute={loc.pathname} />
+        </UIContent>
+      </div>
+    );
+  }
+
+}
 
 export default App;
