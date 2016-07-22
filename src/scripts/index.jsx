3aae5a74c87d5aea09293612edd3d1c5e6601e6a
René Monroy <rene.monroy@gmail.com>
2016-07-22 12:47:48 -0500

    Use routes in the system for transitions better management of scenes

--- src/scripts/index.jsx
+++ src/scripts/index.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './components/';
+import { Router, Route, IndexRedirect, hashHistory } from 'react-router';
+import App from './components';
+import * as Scenes from './components/scenes';
 import configureStore from './utils/configureStore';
 
 const appStore = configureStore();
 
+const routes = (
+  <Route path="/" component={App}>
+    <Route path="/snkr-vote" component={Scenes.SnkrVoteScene} />
+    <IndexRedirect to="/snkr-vote" />
+  </Route>
+);
+
 ReactDOM.render(
   <Provider store={appStore}>
-    <App />
+    <Router history={hashHistory} routes={routes} />
   </Provider>,
   document.getElementById('app-wrapper')
 );
