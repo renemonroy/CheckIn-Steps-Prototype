da2c7d671f6ab1ffd4ff5d222529e6e4229bcb43
René Monroy <rene.monroy@gmail.com>
2016-07-22 12:43:45 -0500

    Create a simple block that says just do it

---
+++ src/scripts/components/blocks/VoteForLoyalty.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class VoteForLoyalty extends Component {
+
+  static displayName = 'VoteForLoyalty';
+
+  render() {
+    return (
+      <div>
+        <p>Just do it!</p>
+      </div>
+    );
+  }
+
+}
+
+export default VoteForLoyalty;
