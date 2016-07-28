0b11b295eb69a92aa267bf530f5bf69c170adc65
René Monroy <rene.monroy@gmail.com>
2016-07-28 14:05:31 -0500

    Install and integrate react-router-transition

--- src/scripts/components/ui/UIScenesGroup.jsx
+++ src/scripts/components/ui/UIScenesGroup.jsx
@@ -1,76 +1,30 @@
+require('./UIScenesGroup.styl');
 import React, { PropTypes } from 'react';
-import _ from 'lodash';
-import { TransitionMotion, spring } from 'react-motion';
-import Radium from 'radium';
-import { fastEaseOut } from '../../constants/SpringPresets';
-let styles = null;
+import { RouteTransition, presets } from 'react-router-transition';
 
-/** UIScenesGroup Class
- *----------------------------------------------------------------------------*/
-@Radium
 class UIScenesGroup extends React.Component {
 
   static displayName = 'UIScenesGroup';
 
   static propTypes = {
-    sceneRoute: PropTypes.string.isRequired,
     scenes: PropTypes.func.isRequired,
-    children: PropTypes.any,
+    sceneRoute: PropTypes.string.isRequired,
   };
 
-  getAnimation() {
-    const { sceneRoute } = this.props;
-    return [{
-      key: sceneRoute,
-      data: this.props.scenes(),
-      style: { opacity: spring(1, fastEaseOut) },
-    }];
-  }
-
-  willEnter() {
-    return { opacity: spring(0, fastEaseOut) };
-  }
-
-  willLeave() {
-    return { opacity: spring(0, fastEaseOut) };
-  }
-
-  handleScenesGroup(interpolatedStyles) {
-    const scenes = _.map(_.keys(interpolatedStyles), (key) => {
-      const sceneAnim = interpolatedStyles[key];
-      const sceneStyle = { opacity: sceneAnim.opacity };
-      const style = [styles.sceneWrapper, sceneStyle];
-      return (
-        <div key={`${key}-scene-trans`} style={style}>
-          {sceneAnim.data}
-        </div>
-      );
-    });
-    return <div id="scenes-group">{scenes}</div>;
-  }
-
   render() {
+    const { sceneRoute, scenes } = this.props;
     return (
-      <TransitionMotion
-        styles={this.getAnimation()}
-        willEnter={::this.willEnter}
-        willLeave={::this.willLeave}
+      <RouteTransition
+        component={'div'}
+        pathname={sceneRoute}
+        className="ui-scenes-group"
+        {...presets.fade}
       >
-        {this.handleScenesGroup.bind(this)}
-      </TransitionMotion>
+        {scenes()}
+      </RouteTransition>
     );
   }
 
 }
-
-/** UIScenesGroup Styles
- *----------------------------------------------------------------------------*/
-styles = {
-  sceneWrapper: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-  },
-};
 
 export default UIScenesGroup;
