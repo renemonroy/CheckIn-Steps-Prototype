91c9d814d029c76135b5a8f508c9d2a88a44906e
René Monroy <rene.monroy@gmail.com>
2016-07-29 14:22:02 -0500

    Change a preset to make the animation more visible in choice buttons

--- src/scripts/constants/SpringPresets.jsx
+++ src/scripts/constants/SpringPresets.jsx
@@ -1,4 +1,4 @@
 export const easeOut = { stiffness: 236, damping: 24 };
 export const fastEaseOut = { stiffness: 330, damping: 28 };
-export const fastEaseOutElastic = { stiffness: 400, damping: 22 };
+export const fastEaseOutElastic = { stiffness: 300, damping: 11 };
 export const slowEaseIn = { stiffness: 53, damping: 14 };
